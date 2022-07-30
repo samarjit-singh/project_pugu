95185caf42cfcf31d059a5a841947ebdc8d6d33a
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-07-31 01:44:41 +0530

    Login register done and cart half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import ProductList from "./pages/ProductList";
+// import Home from "./pages/Home";
+// import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Product />;
+  return <Cart />;
 };
 
 export default App;
