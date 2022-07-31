c0b0bcd76bc0f32bb199dce17f32899dd3f76ffa
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-08-01 02:22:50 +0530

    completed app responsive part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// import Home from "./pages/Home";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,11 @@ import {
   Room,
 } from "@material-ui/icons";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const Left = styled.div`
@@ -43,7 +45,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
   flex: 1;
   padding: 20px;
-  padding: 20px;
+  ${mobile({ display: "none" })}
 `;
 
 const Title = styled.h3`
@@ -65,6 +67,7 @@ const ListItem = styled.li`
 const Right = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
 `;
 
 const ContactItem = styled.div`
